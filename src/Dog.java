d465d8f878ffed1cbc0b260ab162b421d5167d29
MorikuMarieke <mariekevdstaaij@gmail.com>
2024-09-13 12:02:27 +0200

    added files and code

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog {
+    public String name;
+    public int age;
+    public final String species;
+    public final String sex;
+
+    public Dog(String name, int age, String species, String sex) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+        this.sex = sex;
+    }
+
+}
